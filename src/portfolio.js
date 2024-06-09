52a4f39885e2fc9982f8a22a265d5d2be18502da
hahosameer <ks4063180@gmail.com>
2024-06-09 12:37:23 +0500

    cv changes this

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A passionate  Mern stack Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs /  and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/13L94nrO-nGdioiq5Kb-hILMFssMmPzzN/view", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1VURAh05VjYdrPwlVbBAkbeTxZIBRwaqT/view", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
