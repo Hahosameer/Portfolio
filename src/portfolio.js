5d0e8c6ebd2fed0ef868b2db501085651099f673
Sameer Khan <ks4063180@gmail.com>
2026-01-01 11:07:10 +0500

    update resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,7 +26,7 @@ const greeting = {
     "A passionate  Mern stack Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs /  and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1MfPiro08KbQmXlhZdX6AwW1HlXUcJVSW/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1uvLMo7l3LVzkpxj4icgiTcT1gPTSmkuA/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
